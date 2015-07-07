373e63e0b5ba6aeac83d1814dd60aa452d018cf8
thisispete <pete@odopod.com>
2015-07-06 20:02:52 -0700

    grumble grumble express 4 basic auth middleware plugin non workey horseshit...

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-var basicAuth = require('basic-auth-connect');
+var auth = require('http-auth');
 var config = require('nconf');
 var express = require('express');
 var later = require('later');
 var scraperbike = require('./src/scraperbike.js');
+
 
 config.file({ file: 'config.json' }).env().argv();
 
@@ -13,12 +14,18 @@ later.date.localTime();
 var sched = later.parse.recur().every(20).minute().after(8).hour().before(19).hour().onWeekday();
 later.setInterval(scraperbike.fire, sched);
 
-scraperbike.fire();
+
+var basic = auth.basic({
+        realm: config.get('publishTitle')
+    }, function (username, password, callback) {
+        console.log(username, password);
+        console.log(config.get('username'), config.get('password'));
+        callback(username === config.get('username') && password === config.get('password'));
+    }
+);
 
 var app = express();
-app.use(basicAuth(function(u, p) {
- return u === config.get('username') && p === config.get('pasword');
-}));
+app.use(auth.connect(basic));
 
 app.get("/calendar", function(req, res){
   res.header('Content-Type', 'text/calendar; charset=utf-8');
@@ -27,3 +34,4 @@ app.get("/calendar", function(req, res){
 
 var port = process.env.PORT || 5000;
 app.listen(port);
+scraperbike.fire();
